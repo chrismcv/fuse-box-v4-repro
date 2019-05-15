2651c80b19164954d12fc0af4ff26dfef2bb4e09
Gustorn <gustorn@gmail.com>
2019-05-15 20:28:02 +0200

    Initial commit

---
+++ fuse.js
@@ -0,0 +1,18 @@
+const { fusebox } = require("fuse-box");
+
+const fuse = fusebox({
+  target: "browser",
+  entry: "src/index.jsx",
+  webIndex: {
+    template: "src/index.html"
+  },
+  cache: {
+    root: ".cache",
+    enabled: true
+  },
+  watch: true,
+  hmr: true,
+  devServer: true,
+  logging: { level: "succinct" }
+});
+fuse.runDev();
---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Button } from "@material-ui/core";
+
+const element = document.querySelector("#app");
+
+ReactDOM.render(() => <Button />, element);
